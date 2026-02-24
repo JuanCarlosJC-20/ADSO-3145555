86a82614bc4b564f0c3faba86da96c9e1d9b00c7
Juancarlos <sampfz23@gmail.com>
2026-02-24 08:37:09 +0100

    evidencia trabajo semana 1 semana 2

---
+++ 02-week/03-optional-activity/cafeteria-crud/src/main/java/com/sena/cafeteria_crud/dto/Request/userRolDto.java
@@ -0,0 +1,23 @@
+package com.sena.cafeteria_crud.dto.Request;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class userRolDto {
+
+    @NotNull(message = "El id del usuario es obligatorio")
+    private Long userId;
+
+    @NotNull(message = "El id del rol es obligatorio")
+    private Long roleId;
+
+    private Boolean state;
+
+}
